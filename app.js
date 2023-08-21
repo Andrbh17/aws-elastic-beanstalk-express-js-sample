bd6ddc85df4339809576e9453c25b65cf61dbbe5
Halvorsen <andrehalvorsen@Andres-MacBook-Pro.local>
2023-08-21 14:10:45 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Halla! hva skjer?'));
+app.get('/', (req, res) => res.send('Hei på deg :)'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
